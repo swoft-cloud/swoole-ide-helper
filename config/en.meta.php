cbb3853a87be76e18b957b8b480a49e242f4ec9e
inhere <in.798@qq.com>
2019-09-25 18:35:47 +0800

    up

--- config/en.meta.php
+++ config/en.meta.php
@@ -11,7 +11,7 @@ return [
         'server_socket' => 'int(-1):This parameter is required when sending data to the Unix Socket DGRAM peer.
         The TCP client does not need to fill in',
     ],
-    //Func:function
+    // Func:function
     'Func:swoole_version' => [
         '_desc' => 'return swoole version. eg: 4.4.1'
     ],
